f4c023b70403020438f5dadf444c69460b0cfb49
Tiago Freitas <freitastiago1910@gmail.com>
2024-02-07 14:18:05 -0300

    Fixed functionality and added Restart button

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const mainContainer = document.querySelector(".main-container");
 const startButton = document.getElementById("button");
 let blockAmountCounter = 0;
+const resetButton = document.getElementById("resetButton");
 
 startButton.addEventListener('click', function () {
     const blockAmount = getUserPrompt();
@@ -8,7 +9,8 @@ startButton.addEventListener('click', function () {
     console.log("Button clicked!");
 });
 
-function getUserPrompt() {
+function getUserPrompt() 
+{
     if (blockAmountCounter > 0) {
         alert("Please, restart the page with F5.");
         return 0;
@@ -24,7 +26,8 @@ function getUserPrompt() {
     }
 }
 
-function generateBlocks(blockAmount) {
+function generateBlocks(blockAmount) 
+{
     for (let i = 0; i < blockAmount; i++) {
         const squareRoot = Math.sqrt(blockAmount)
         const newBlock = document.createElement('div');
@@ -32,5 +35,21 @@ function generateBlocks(blockAmount) {
         newBlock.style.height = `calc(100% / ${squareRoot})`;
         newBlock.style.width =  `calc(100% / ${squareRoot})`;
         mainContainer.appendChild(newBlock);
+
+
+        newBlock.addEventListener("mouseover", function(){
+            changeColor(this)
+        })
     }
 }
+
+function changeColor(element)
+{
+    element.style.backgroundColor = "#000000"
+}
+
+resetButton.addEventListener("click", function()
+{
+    blockAmountCounter = 0;
+    mainContainer.innerHTML = '';
+});
